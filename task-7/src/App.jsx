dacf3a87db80c1577904af4d8d5f7d3ee89b3d36
P D Prajwal <pdprajwal9522@gmail.com>
2026-05-21 00:35:25 +0530

    Initial commit: added task folders and files

---
+++ task-7/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Design1 from './components/designbox1/design1'
+function App() {
+ return (
+    <>
+      <Design1/>
+
+    </>
+  
+  )
+}
+
+export default App
---
+++ task-7/src/components/designbox1/design1.jsx
@@ -0,0 +1,66 @@
+import './design1.css'
+
+function Design1(){
+    return(
+<>
+
+<div className="container">
+    <div className="nav">
+<div className="logo"><h2>LOGO</h2></div>
+
+<div className="navbar">
+    <a href = '#'>Home</a>
+     <a href = '#'>Interface</a>
+      <a href = '#'>Interface</a>
+       <a href = '#'>Image 5/10-wir</a>
+        <a href = '#'>Tips</a>
+        <a href = '#'>Summary</a>
+        <a href = '#'>Sponsorship</a>
+        <a href = '#'>Contact</a>
+         
+
+
+
+</div>
+
+    </div>
+
+    <div className="box1">
+        <div className="adspace"><h2>Ad <br></br>Space</h2> </div>
+        <div className="rotater"><h2>Rotator</h2></div>
+    </div>
+  
+    <div className="box2">
+        <div className="featured">
+<div className="child"></div>
+        
+        <div className="textwrite"><h2>Featured <br></br>Child</h2></div>
+       </div>
+        <div className="simple"><h2>Simple Give Area</h2></div>
+    </div>
+    <div className="box3">
+        <div className="sponser"><h2>sponser</h2></div>
+        <div className="creative"><h2>Creative Ways to Give</h2></div>
+
+    </div>
+<div className="box4">
+    <div className="boxes">
+        <div className="b1"></div>
+        <div className="b2"></div>
+        <div className="b3"></div>
+        <div className="b4"></div>
+        <div className="b5"></div>
+    </div>
+    <div className="text"><h5>Pics + Mini Bios of Children</h5></div>
+
+</div>
+
+
+</div>
+
+</>
+    )
+}
+
+
+export default Design1
