b255b31c0c65a73fe50c990100655726ebb38c4a
P D Prajwal <pdprajwal9522@gmail.com>
2026-05-23 18:23:58 +0530

    added design 6

--- task-7/src/App.jsx
+++ task-7/src/App.jsx
@@ -4,11 +4,12 @@ import './App.css'
 //import Design2 from './components/designbox2/design2'
 //import Design3 from './components/designbox3/design3'
 //import Design4 from './components/designbox4/design4'
-import Design5 from './components/designbox5/design5'
+//import Design5 from './components/designbox5/design5'
+import Design6 from './components/designbox6/design6'
 function App() {
  return (
     <>
-     <Design5/>
+     <Design6/>
 
     </>
   
