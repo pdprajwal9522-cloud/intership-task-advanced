6b7599646a490f79c634367d62b6bddded4d3acf
P D Prajwal <pdprajwal9522@gmail.com>
2026-05-24 19:46:32 +0530

    added design 9

--- task-7/src/App.jsx
+++ task-7/src/App.jsx
@@ -7,11 +7,12 @@ import './App.css'
 //import Design5 from './components/designbox5/design5'
 //import Design6 from './components/designbox6/design6'
 //import Design7 from './components/designbox7/design7'
-import Design8 from './components/designbox8/design8'
+//import Design8 from './components/designbox8/design8'
+import Design9 from './components/designbox9/design9'
 function App() {
  return (
     <>
-     <Design8/>
+     <Design9/>
 
     </>
   
---
+++ task-7/src/components/designbox9/design9.jsx
@@ -0,0 +1,70 @@
+import './design9.css'
+import buttonIcon from '../../../public/icon.png';
+function Design9(){
+    return(
+        <>
+        <div className="container">
+    <div className="nav">
+<div className="logo"><h2>LOGO</h2></div>
+
+<div className="navbar">
+    <a href = '#'>Home</a>
+     <a href = '#'>Interface</a>
+      <a href = '#'>Image 5/10-wir</a>
+        <a href = '#'>Tips</a>
+        <a href = '#'>Summary</a>
+        <a href = '#'>Sponsorship</a>
+        <a href = '#'>Contact</a>
+         </div>
+ </div>
+<div className="box1">
+        <div className="adspace"><h2>Ad <br></br>Space</h2> </div>
+        <div className="rotater"><h2>Rotator</h2></div>
+    </div>
+  
+    <div className="box2">
+        <div className="top-div">
+            <div className="icon-1">
+                <img src ={buttonIcon} alt= "icon-buttonIcon"/>
+            </div>
+            <div className="fun-1"><h4>Fundrainer idea + Text</h4></div>
+        </div>
+        <div className="middle-div">
+            <div className="icon-2">
+                <img src ={buttonIcon} alt= "icon-buttonIcon"/>
+            </div>
+            <div className="fun-2"><h4>Fundrainer idea + Text</h4></div>
+        </div>
+        <div className="last-div">
+            <div className="icon-3">
+                <img src ={buttonIcon} alt= "icon-buttonIcon"/>
+            </div>
+            <div className="fun-3"><h4>Fundrainer idea + Text</h4></div>
+        </div>
+
+
+        
+      </div>
+      
+     
+        
+      
+<div className="box3">
+    <div className="boxes">
+        <div className="b1"></div>
+        <div className="b2"></div>
+        <div className="b3"></div>
+        <div className="b4"></div>
+        <div className="b5"></div>
+    </div>
+    <div className="text"><h5>Pics + Mini Bios of Children</h5></div>
+</div>
+
+
+</div>
+    
+
+        </>
+    )
+}
+export default Design9
